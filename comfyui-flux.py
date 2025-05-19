658453daf30627477e0afbeb8afedcd5579b038d
ivan <aureliusivanwijaya@gmail.com>
2025-05-20 00:35:57 +0700

    Update comfyui-flux.py to include Civitai API token in model download commands and enhance secrets management for improved security.

--- comfyui-flux.py
+++ comfyui-flux.py
@@ -17,17 +17,21 @@ flux = (  # Download image layers to run FLUX_Q8.gguf model
         "comfy --skip-prompt install --nvidia",
     )
     .run_commands(# download the GGUF Q8 model
-    "comfy --skip-prompt model download --url https://huggingface.co/city96/FLUX.1-dev-gguf/resolve/main/flux1-dev-Q8_0.gguf  --relative-path models/unet",
+    "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/city96/FLUX.1-dev-gguf/resolve/main/flux1-dev-Q8_0.gguf  --relative-path models/unet",
+    secrets=[modal.Secret.from_name("civitai-api-token")]
     )
     .run_commands( # gguf node required for q8 model
         "comfy node install https://github.com/city96/ComfyUI-GGUF"
     )
     .run_commands(  # download the vae model required to use with the gguf model
-        "comfy --skip-prompt model download --url https://huggingface.co/black-forest-labs/FLUX.1-schnell/resolve/main/ae.safetensors --relative-path models/vae"
+        # "comfy --skip-prompt model download --url https://huggingface.co/black-forest-labs/FLUX.1-schnell/resolve/main/ae.safetensors --relative-path models/vae"
+        "comfy --skip-prompt model download --url https://civitai.com/api/download/models/928242 --relative-path models/vae --set-civitai-api-token $CIVITAI_API_TOKEN",
+        secrets=[modal.Secret.from_name("civitai-api-token")]
     )
     .run_commands(  # download the cliper model required to use with GGUF model
-        "comfy --skip-prompt model download --url https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors --relative-path models/clip",
-        "comfy --skip-prompt model download --url https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors --relative-path models/clip",
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors --relative-path models/clip",
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors --relative-path models/clip",
+    secrets=[modal.Secret.from_name("civitai-api-token")]
     )
     .run_commands(  # download the lora anime -- optional you can disbale
         "comfy --skip-prompt model download --url https://civitai.com/api/download/models/716064 --relative-path models/loras --set-civitai-api-token $CIVITAI_API_TOKEN",
@@ -38,24 +42,27 @@ flux = (  # Download image layers to run FLUX_Q8.gguf model
         "comfy node install https://github.com/XLabs-AI/x-flux-comfyui"
     )
     .run_commands( #download controlnet v3 xlabs ai
-        "comfy --skip-prompt model download --url https://huggingface.co/XLabs-AI/flux-controlnet-depth-v3/resolve/main/flux-depth-controlnet-v3.safetensors --relative-path models/xlabs/controlnets",
-        "comfy --skip-prompt model download --url https://huggingface.co/XLabs-AI/flux-controlnet-canny-v3/resolve/main/flux-canny-controlnet-v3.safetensors --relative-path models/xlabs/controlnets",
-        "comfy --skip-prompt model download --url https://huggingface.co/XLabs-AI/flux-controlnet-hed-v3/resolve/main/flux-hed-controlnet-v3.safetensors --relative-path models/xlabs/controlnets",
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/XLabs-AI/flux-controlnet-depth-v3/resolve/main/flux-depth-controlnet-v3.safetensors --relative-path models/xlabs/controlnets",
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/XLabs-AI/flux-controlnet-canny-v3/resolve/main/flux-canny-controlnet-v3.safetensors --relative-path models/xlabs/controlnets",
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/XLabs-AI/flux-controlnet-hed-v3/resolve/main/flux-hed-controlnet-v3.safetensors --relative-path models/xlabs/controlnets",
+    secrets=[modal.Secret.from_name("civitai-api-token")]
     )
     .run_commands( #install control net requried for above xlabs
         "comfy node install https://github.com/Fannovel16/comfyui_controlnet_aux"
     )
     .run_commands( #xlab loras --optional
-        "comfy --skip-prompt model download --url https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/art_lora_comfy_converted.safetensors --relative-path models/loras",
-        "comfy --skip-prompt model download --url https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/anime_lora_comfy_converted.safetensors --relative-path models/loras",
-        "comfy --skip-prompt model download --url https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/disney_lora_comfy_converted.safetensors --relative-path models/loras",
-        "comfy --skip-prompt model download --url https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/mjv6_lora_comfy_converted.safetensors --relative-path models/loras",
-        "comfy --skip-prompt model download --url https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/realism_lora_comfy_converted.safetensors --relative-path models/loras",
-        "comfy --skip-prompt model download --url https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/scenery_lora_comfy_converted.safetensors --relative-path models/loras"
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/art_lora_comfy_converted.safetensors --relative-path models/loras",
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/anime_lora_comfy_converted.safetensors --relative-path models/loras",
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/disney_lora_comfy_converted.safetensors --relative-path models/loras",
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/mjv6_lora_comfy_converted.safetensors --relative-path models/loras",
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/realism_lora_comfy_converted.safetensors --relative-path models/loras",
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/scenery_lora_comfy_converted.safetensors --relative-path models/loras",
+    secrets=[modal.Secret.from_name("civitai-api-token")]
     )
     .run_commands( #someloras optional
-        "comfy --skip-prompt model download --url https://huggingface.co/alvdansen/frosting_lane_flux/resolve/main/flux_dev_frostinglane_araminta_k.safetensors --relative-path models/loras",
-        "comfy --skip-prompt model download --url https://huggingface.co/multimodalart/flux-tarot-v1/resolve/main/flux_tarot_v1_lora.safetensors --relative-path models/loras",
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/alvdansen/frosting_lane_flux/resolve/main/flux_dev_frostinglane_araminta_k.safetensors --relative-path models/loras",
+        "comfy --skip-prompt model download --set-hf-api-token $HF_API_TOKEN --url https://huggingface.co/multimodalart/flux-tarot-v1/resolve/main/flux_tarot_v1_lora.safetensors --relative-path models/loras",
+    secrets=[modal.Secret.from_name("civitai-api-token")]
     )
     .run_commands( #CR APPLY lora stack -- useful node -- optional
         "comfy node install https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes"
@@ -70,7 +77,9 @@ app = modal.App(name="flux-comfyui", image=flux)
     timeout=3200,
     # gpu="a10g", # here you can change the gpu, i recommend either a10g or T4
     gpu="T4",
-    secrets=[modal.Secret.from_name("civitai-api-token")],
+    secrets=[
+        modal.Secret.from_name("civitai-api-token"), 
+    ],
 )
 @modal.concurrent(max_inputs=10)
 @modal.web_server(8000, startup_timeout=60)
